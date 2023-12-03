2ba6c2eaf39eb49e8727aa50742c31d161f474e5
marcuswff <marcus.wallberg@forefront.se>
2023-12-03 11:44:41 +0100

    day1

--- advent23/src/utils.rs
+++ advent23/src/utils.rs
@@ -1,3 +1,14 @@
-pub fn test() {
-    println!("Hello!");
+use std::{
+    fs::File,
+    io::{prelude::*, BufReader},
+    path::Path,
+};
+
+pub fn read_file(filename: impl AsRef<Path>) -> Vec<String> {
+    let file = File::open(filename).expect("no such file");
+    let buf = BufReader::new(file);
+    buf.lines()
+        .map(|l| l.expect("Could not parse line"))
+        .collect()
 }
+
